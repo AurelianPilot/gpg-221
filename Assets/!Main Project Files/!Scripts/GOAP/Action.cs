485d799566fdddb274310a79293d3bc2e3b4a185
Leonardo Estigarribia <salestigarribia@gmail.com>
2025-03-24 18:57:41 +0400

    Updated the Action class (WIP)

--- Assets/!Main Project Files/!Scripts/GOAP/Action.cs
+++ Assets/!Main Project Files/!Scripts/GOAP/Action.cs
@@ -1,10 +1,11 @@
 using System.Collections.Generic;
+using _Main_Project_Files._Scripts.Pathfinding;
 using UnityEngine;
 
 namespace _Main_Project_Files._Scripts.GOAP
 {
     /// <summary>
-    /// An action that is performed by AI.
+    /// An action that is performed by AI (as in, an agent).
     /// </summary>
     public class Action : MonoBehaviour
     {
@@ -22,7 +23,44 @@ namespace _Main_Project_Files._Scripts.GOAP
         /// </summary>
         [SerializeField] protected List<PreRequisite> preRequisites = new List<PreRequisite>();
 
+        // Getters.
+        public string ActionName => actionName;
+        public float ActionCost => actionCost;
+        public bool IsActionAchivable => isActionAchivable;
+        public List<PreRequisite> PreRequisites => preRequisites;
+        public List<Effect> Effects => effects;
+        
         #region Script Specific
+
+        /// <summary>
+        ///  Checks the world state lists if the preRequisites are met to perform an action.
+        /// </summary>
+        /// <param name="worldState">Reference to the world state.</param>
+        /// <returns></returns>
+        public bool ArePreRequisitesSatisfied(WorldState worldState)
+        {
+            if (!isActionAchivable) return false;
+            
+            foreach (var preRequisite in preRequisites)
+            {
+                if (!preRequisite.IsSatisfied(worldState)) return false;
+            }
+
+            return true;
+        }
+
+        /// <summary>
+        /// Apply all effects to the world state.
+        /// </summary>
+        /// <param name="worldState">Reference to the world state.</param>
+        public void ApplyEffects(WorldState worldState)
+        {
+            foreach (var effect in effects)
+            {
+                effect.ApplyEffect(worldState);
+            }
+        }
+        
         /// <summary>
         /// Get cost of than action.
         /// </summary>
@@ -33,14 +71,7 @@ namespace _Main_Project_Files._Scripts.GOAP
             // TODO: Make sure to return the actual value later in development.
             return 0;
         }
-
-        /// <summary>
-        /// Perform the action.
-        /// </summary>
-        public void PerformAction()
-        {
-            throw new System.NotImplementedException();
-        }
+        
         #endregion
 
     }

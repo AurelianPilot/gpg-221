75ec838fb1f97b0b4cb4499476a877a125e35698
Leonardo Estigarribia <salestigarribia@gmail.com>
2025-03-24 18:21:29 +0400

    Finished the effects structure.

--- Assets/!Main Project Files/!Scripts/GOAP/Effect.cs
+++ Assets/!Main Project Files/!Scripts/GOAP/Effect.cs
@@ -5,12 +5,21 @@ namespace _Main_Project_Files._Scripts.GOAP
 {
     public class Effect : MonoBehaviour
     {
-        [SerializeField] private bool effectActive = true;
+        [SerializeField] private bool isEffectActive = true;
         [SerializeField] private string effectName = "";
+        
+        public bool IsEffectActive => isEffectActive;
+        public string EffectName => effectName;
 
+        public Effect(string effectName, bool isEffectActive)
+        {
+            this.isEffectActive = isEffectActive;
+            this.effectName = effectName;
+        }
+        
         public void ApplyEffect(WorldState worldState)
         {
-            
+            worldState.SetState(effectName, isEffectActive);
         }
     }
 }
